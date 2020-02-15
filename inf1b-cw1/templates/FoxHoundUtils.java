59eaa64e776ac1e5e190aec167222ac9e99967a0
MeinaLinWei <meina.lw12@gmail.com>
2020-02-15 03:28:27 +0000

    Task 1 and 3 done.

--- inf1b-cw1/templates/FoxHoundUtils.java
+++ inf1b-cw1/templates/FoxHoundUtils.java
@@ -91,4 +91,76 @@ public class FoxHoundUtils {
 
     }
 
+    public static boolean isValidMove(int dim, String[] players, char figure, String origin, String destination){
+
+        // Get the letter part of the initial position.
+        char rowOrigin = origin.charAt(0);
+        // Get the number part of the initial position.
+        int numOrigin = Integer.parseInt(origin.substring(1));
+
+        // Calculate the new letter of the destination.
+        char rightUp = (char)(rowOrigin+1);
+        char rightDown = (char)(rowOrigin+1);
+        char leftUp = (char)(rowOrigin-1);
+        char leftDown = (char)(rowOrigin-1);
+
+        // Calculate possible destination.
+        String finalRightUp = (rightUp+""+(numOrigin-1));
+        String finalRightDown = (rightDown+""+(numOrigin+1)); // fox can move in all four directions.
+        String finalLeftUp = (leftUp+""+(numOrigin-1));
+        String finalLeftDown = (leftDown+""+(numOrigin+1)); // fox can move in all four directions.
+
+        boolean correctDestination = true;
+        if ((isElement(dim, players, origin, destination)) == true){
+            switch (figure) {
+                case 'F':
+                    if ((finalRightUp.equals(destination) || (finalRightDown.equals(destination)) ||
+                            (finalLeftUp.equals(destination)) || (finalLeftDown.equals(destination))) == true) {
+                        correctDestination = true;
+                    } else {
+                        correctDestination = false;
+                    }
+                    break;
+                case 'H':
+                    if (((finalRightDown.equals(destination)) || (finalLeftDown.equals(destination))) == true) {
+                        correctDestination = true;
+                    } else {
+                        correctDestination = false;
+                    }
+                    break;
+            }
+        } else {
+            correctDestination = false;
+        }
+        return correctDestination;
+    }
+
+    // Check if destination coordinate is on board and if original coordinate is either Hound or Fox
+    public static boolean isElement(int dim, String[] players, String origin, String destination){
+        boolean result1 = true;
+        boolean result2 = true;
+
+        // Get number and letter of final position.
+        int numDest = Integer.parseInt(destination.substring(1));
+        char rowDest = destination.charAt(0);
+
+        // Check that original position on board contains either Hound or Fox.
+        for(int count = 0; count < players.length; count ++){
+            if(players[count].equals(origin)){
+                result1 = true;
+                break; // terminate loop if original position contain either Hound or Fox.
+            } else {
+                result1 = false;
+            }
+        }
+
+        // Check final destination coordinates exist on given the dimension of the board.
+        if(numDest > dim || rowDest > ((char)('A'+ (dim-1)))){
+            result2 =  false;
+        } else {
+            result2 = true;
+        }
+        return (result1 && result2); // Check that both conditions are true.
+    }
+
 }
